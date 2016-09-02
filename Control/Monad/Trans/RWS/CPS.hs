a5340a5dcf5b8ceeef83a591973bfe81e8508fe2
Daniel Mendler <mail@daniel-mendler.de>
2016-09-02 08:24:16 +0200

    add docs

--- Control/Monad/Trans/RWS/CPS.hs
+++ Control/Monad/Trans/RWS/CPS.hs
@@ -5,6 +5,24 @@
 #if __GLASGOW_HASKELL__ >= 710
 {-# LANGUAGE AutoDeriveTypeable #-}
 #endif
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.Trans.RWS.CPS
+-- Copyright   :  (c) Daniel Mendler 2016,
+--                (c) Andy Gill 2001,
+--                (c) Oregon Graduate Institute of Science and Technology, 2001
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  mail@daniel-mendler.de
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- A monad transformer that combines 'ReaderT', 'WriterT' and 'StateT'.
+-- This version uses continuation-passing-style for the writer part
+-- to achieve constant space usage. This transformer can be used as a
+-- drop-in replacement for Control.Monad.Trans.RWS.Strict/Lazy.
+-----------------------------------------------------------------------------
+
 module Control.Monad.Trans.RWS.CPS (
   -- * The RWS monda
   RWS,
--- Control/Monad/Trans/Writer/CPS.hs
+++ Control/Monad/Trans/Writer/CPS.hs
@@ -5,6 +5,30 @@
 #if __GLASGOW_HASKELL__ >= 710
 {-# LANGUAGE AutoDeriveTypeable #-}
 #endif
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.Trans.Writer.CPS
+-- Copyright   :  (c) Daniel Mendler 2016,
+--                (c) Andy Gill 2001,
+--                (c) Oregon Graduate Institute of Science and Technology, 2001
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  mail@daniel-mendler.de
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- The strict 'WriterT' monad transformer, which adds collection of
+-- outputs (such as a count or string output) to a given monad.
+--
+-- This monad transformer provides only limited access to the output
+-- during the computation. For more general access, use
+-- "Control.Monad.Trans.State" instead.
+--
+-- This version builds its output strictly and uses continuation-passing-style
+-- to achieve constant space usage. This transformer can be used as a
+-- drop-in replacement for Control.Monad.Trans.Writer.Strict/Lazy.
+-----------------------------------------------------------------------------
+
 module Control.Monad.Trans.Writer.CPS (
   -- * The Writer monad
   Writer,
